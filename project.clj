b63ab981a30f37e2f4b7e4556ba177c7a442ed2f
Jeff Rose <rosejn@gmail.com>
2015-12-04 13:01:36 -0500

    Minor updates to use re-arranged core.matrix namespaces

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 ;; This file is proveded as a convenience for Leiningen users
 ;;
-;; The pom.xml is used for official builds, and should be considered the 
+;; The pom.xml is used for official builds, and should be considered the
 ;; definitive source for build configuration.
 ;;
 ;; If you are having trouble building, please check the pom.xml for latest dependency versions
 
-(defproject net.mikera/vectorz-clj "0.30.2-SNAPSHOT"
+(defproject net.mikera/vectorz-clj "0.37.0-SNAPSHOT"
   :description "Fast vector library for Clojure, building on Vectorz and using core.matrix"
   :url "https://github.com/mikera/vectorz-clj"
   :license {:name "GNU Lesser General Public License (LGPL)"
@@ -18,12 +18,12 @@
                  [org.clojure/test.check "0.7.0"]
                  [net.mikera/cljunit "0.3.1"]
                  [net.mikera/clojure-utils "0.6.2"]
-                 [net.mikera/core.matrix "0.36.1"]
+                 [net.mikera/core.matrix "0.45.0-CLJS-SNAPSHOT"]
                  [net.mikera/core.matrix.testing "0.0.4"]
                  [net.mikera/core.matrix.stats "0.4.0"]
                  [net.mikera/vectorz "0.47.0"]]
-  
+
   :profiles {:dev {:java-source-paths ["src/test/java"]}}
-  
+
   :repositories [["clojars.org" {:url "https://clojars.org/repo"
                                  :name "Clojars repository"}]])
